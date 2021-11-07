c3152eafb814d12b67d54d2057e600a3ccab681c
20WH1A6653 <93580510+20WH1A6653@users.noreply.github.com>
2021-11-07 17:40:21 +0530

    Update binary_rec.c

--- binary_rec.c
+++ binary_rec.c
@@ -1,31 +1,33 @@
 #include<stdio.h>
 int binary_search(int [],int,int,int );
 int main(){
-         int a[50],n  ,i,ans,key;
+         int a[50],n,i,ans,keylow,high;
          printf("enter array size:");
          scanf("%d",&n);
          printf("enter array elements");
          for(i=0;i<n;i++){
            scanf("%d",&a[i]);
         }
+        low=0;
+        high=n-1;
         printf("enter element to be searched");
         scanf("%d",&key);
-        ans = binary_search(a,0,n-1,key);
+        ans = binary_search(a,low,high,key);
         if(ans == -1)
           printf("element not found");
         else
           printf("element found at %d",ans+1);
 }
 int binary_search(int a[],int low,int high,int key){
-int mid;
-    mid = (low+high)/2;
-    while(low<=high){
-
-      if(key < a[mid])
-        binary_search(a,low,mid-1,key);
-      else if(key > a[mid])
-        binary_search(a,mid+1,high,key);
-      else
+   int mid;
+   if(low>high)
+     return -1;
+   mid = (low+high)/2;
+   if(key == a[mid]){
         return mid;
-}return -1;
+   }else if(key > a[mid]){
+        binary_search(a,mid+1,high,key);
+    }else{
+        binary_search(a,low,mid-1,key);
+}
 }
